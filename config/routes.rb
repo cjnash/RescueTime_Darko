7185bc86dd7c784db1b929b26243b6e99f44c36a
Doug Stewart <doug.stewart@ama.ab.ca>
2010-12-15 20:16:14 -0700

    Removed index.html to allow the index.html.erb in the /views/darkos area to become the real index. Set root :to => darkos#index in routes.rb to have that index file be the 'home' page

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ RescueTimeDarko::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "darkos#index"
 
   # See how all your routes lay out with "rake routes"
 
