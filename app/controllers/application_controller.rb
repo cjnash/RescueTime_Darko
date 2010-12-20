e86ef454312fbb63daeff4307b687b9c41ce4e82
cjnash <nashcjnash@mac.com>
2010-12-19 21:17:40 -0700

    fixed time zone, tweaked css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_timezone
+
+  def set_timezone
+    # current_user.time_zone #=> 'London'
+    Time.zone = 'Mountain Time (US & Canada)'
+  end
+  
 end
